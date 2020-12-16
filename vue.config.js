b7a74590ae069dd4cfe33023ce3d8854bcb52034
machinesily <934229118@qq.com>
2020-12-16 18:49:44 +0800

    第三次上线

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,6 @@ module.exports = {
             .set('common',resolve('src/common'))
             .set('views',resolve('src/views'))
             .set('network',resolve('src/network'))
-    }
+    },
+    publicPath:"./"
 };
